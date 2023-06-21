ed158d66f13b257b352bbf459dfcd02fb2039daa
parksomin72 <fatimazahraealawi@gmail.com>
2023-06-21 12:07:55 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,21 +1,41 @@
 #include <stdio.h>
 /**
- * main -Print the sun of Even fibonacci numbers
- * less than 4000000
- * Return: Nothing!
+ * main - fibonacci <3
+ *
+ * Purpose - no hardcode
+ *
+ * Return:  (Success)
  */
-int main(viod)
+int main(void)
 {
-int i =0;
-log j =1, k =2, sum = k;
-while (k+j < 4000000)
+unsigned long int i;
+unsigned long int bef = 1;
+unsigned long int aft = 2;
+unsigned long int l = 1000000000;
+unsigned long int bef1;
+unsigned long int bef2;
+unsigned long int aft1;
+unsigned long int aft2;
+printf("%lu", bef);
+for (i = 1; i < 91; i++)
 {
-k += j;
-if (k % 2 == 0)
-sum += k;
-j = k - j;
-++i;
+printf(", %lu", aft);
+aft += bef;
+bef = aft - bef;
 }
-printf("%ld\n", sum);
+bef1 = (bef / l);
+bef2 = (bef % l);
+aft1 = (aft / l);
+aft2 = (aft % l);
+for (i = 92; i < 99; ++i)
+{
+printf(", %lu", aft1 + (aft2 / l));
+printf("%lu", aft2 % l);
+aft1 = aft1 + bef1;
+bef1 = aft1 - bef1;
+aft2 = aft2 + bef2;
+bef2 = aft2 - bef2;
+}
+printf("\n");
 return (0);
 }
