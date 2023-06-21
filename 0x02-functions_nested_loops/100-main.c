6508503371167d6df93c12fb1c659699f967fc21
parksomin72 <fatimazahraealawi@gmail.com>
2023-06-20 19:26:14 -0700

    times table

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-print_time_table(3);
+print_times_table(3);
 _putchar('\n');
-print_time_table(5);
+print_times_table(5);
 _putchar('\n');
-print_time_table(98);
+print_times_table(98);
 _putchar('\n');
-print_time_table(12);
+print_times_table(12);  
 return (0);
 }
