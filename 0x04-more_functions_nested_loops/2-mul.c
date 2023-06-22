4f048de7e4d7f06681da003436b303df29fdc2f4
parksomin72 <fatimazahraealawi@gmail.com>
2023-06-22 02:11:25 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.
+/**
+ * mul - multiply 2 int
+ * @a: first int
+ * @b: second int
+ * Return: addition of the multiplicattion
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,11 @@
+#iclude "main.h"
+/**
+ * main - check the code
+ *
+ * Returm: Always 0.
+ */
+int main(void)
+{
+print_most_numbers();
+return (0);
+}
