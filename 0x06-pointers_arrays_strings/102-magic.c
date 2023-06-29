2ee8676f0dd80b89ae13843a2c4c71db74f6b561
parksomin72 <fatimazahraealawi@gmail.com>
2023-06-29 04:24:20 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+/*
+* write your line of code here...
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code.
+* -The main function is the entry point of the program.
+* -It initializes variables, performs calculations, and prints the result.
+ */
 int main(void)
 {
 int n;
@@ -6,16 +16,7 @@ int a[5];
 int *p;
 a[2] = 1024;
 p = &n;
-/*
-* write your line of code here...
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statement
-* - you are not allowed to code anything else than this line of code
-*/
-*(p + 5) = 98;
-/* ...so that this prints 98\n */
+*(p + 7) = 98;
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
