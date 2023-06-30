0defb3b9dff2efa84ebcfdd4dc0905a7a9b49daa
parksomin72 <fatimazahraealawi@gmail.com>
2023-06-30 16:48:27 -0700

    magic

--- 0x06-pointers_arrays_strings/103-main.c
+++ 0x06-pointers_arrays_strings/103-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * main - check the code
  *
@@ -7,40 +8,58 @@
  */
 int main(void)
 {
-char *n = "123456789243457436782357457567847768578564568587687677458673473456";
-char *m = "903479066347069723468291456934625963495869324659732465976234795634";
-char r[100];
-char r2[10];
-char r3[11];
-char *res;
-res = infinite_add(n, m, r, 100);
-if (res == 0)
-{
-printf("Error\n");
-}
-else
-{
-printf("%s + %s = %s\n", n, m, res);
-}
-n = "1234567890";
-m = "1";
-res = infinite_add(n, m, r2, 10);
-if (res == 0)
-{
-printf("Error\n");
-}
-else
-{
-printf("%s + %s = %s\n", n, m, res);
-}
-res = infinite_add(n, m, r3, 11);
-if (res == 0)
-{
-printf("Error\n");
-}
-else
-{
-printf("%s + %s = %s\n", n, m, res);
-}
-return (0);
+    char *n = "1234567892434574367823574575678477685785645685876876774586734734563"
+               "456453743756756784458";
+    char *m = "9034790663470697234682914569346259634958693246597324659762347956349"
+               "265983465962349569346";
+    char r[100];
+    char r2[10];
+    char r3[11];
+    char *res;
+
+    res = infinite_add(n, m, r, 100);
+    if (res == 0)
+    {
+        printf("Error\n");
+    }
+    else
+    {
+        printf("%s + %s = %s\n", n, m, res);
+    }
+
+    n = "1234567890";
+    m = "1";
+    res = infinite_add(n, m, r2, 10);
+    if (res == 0)
+    {
+        printf("Error\n");
+    }
+    else
+    {
+        printf("%s + %s = %s\n", n, m, res);
+    }
+
+    n = "999999999";
+    m = "1";
+    res = infinite_add(n, m, r2, 10);
+    if (res == 0)
+    {
+        printf("Error\n");
+    }
+    else
+    {
+        printf("%s + %s = %s\n", n, m, res);
+    }
+
+    res = infinite_add(n, m, r3, 11);
+    if (res == 0)
+    {
+        printf("Error\n");
+    }
+    else
+    {
+        printf("%s + %s = %s\n", n, m, res);
+    }
+
+    return 0;
 }
