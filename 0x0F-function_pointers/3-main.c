15254e4703d9ee518bb1653a0a9783bcf7618a44
parksomin72 <fatimazahraealawi@gmail.com>
2023-07-19 16:42:43 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,4 @@
-#iclude "function_pointers.h"
+#include "function_pointers.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include "3-calc.h"
@@ -9,7 +9,7 @@
  *
  * Return: Always 0.
  */
-int main(int __atribute__((__unused__)) argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 int num1, num2;
 char *op;
