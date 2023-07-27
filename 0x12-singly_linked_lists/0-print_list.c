7f48b69672ab993c054e21a0af373888a0960d2f
parksomin72 <fatimazahraealawi@gmail.com>
2023-07-27 11:38:34 -0700

    New commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,18 +10,18 @@ size_t print_list(const list_t *h)
 {
 size_t s = 0;
 
-	while (h)
-	{
-		if (!h->str)
-			printf("[0] (nill)\n");
+while (h)
+{
+if (!h->str)
+printf("[0] (nill)\n");
 
-		else
-			printf("[%u] %s\n", h->len, h->str);
+else
+printf("[%u] %s\n", h->len, h->str);
 
-		h = h->next;
+h = h->next;
 
-		s++;
+s++;
 }
 
-	return (s);
+return (s);
 }
