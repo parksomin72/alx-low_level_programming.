235683a8f5f39315db79b83e1ad8687373a8b72b
parksomin72 <fatimazahraealawi@gmail.com>
2023-07-27 12:54:41 -0700

    new commit

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,9 @@
-section .data
-hello_msg db "Hello, Holberton", 0
-format db "%s", 10, 0
-section .text
-extern printf
-global main
+global    main
+extern    printf
 main:
-mov rdi, format
-mov rsi, hello_msg
-xor rax, rax       ; Clear rax to indicate no floating-point arguments
-call printf
-; Exit the program
-mov eax, 60         ; System call number for exit
-xor edi, edi        ; Return status 0
-syscall
+mov   edi, format
+xor   eax
+call  print
+mov         eax, 0
+ret
+format: db `Hello, Holberton\n`,0
