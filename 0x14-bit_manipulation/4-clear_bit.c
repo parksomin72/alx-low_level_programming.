59b30d3e06f6135c08b1fe5a2ab487d6c0df490d
parksomin72 <fatimazahraealawi@gmail.com>
2023-08-03 03:54:35 -0700

    New commit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,9 +8,9 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+unsigned long int mask = 1UL << index;
 if (index >= sizeof(unsigned long int) * 8)
 return (-1);
-unsigned long int mask = 1UL << index;
 mask = ~mask;
 *n &= mask;
 return (1);
