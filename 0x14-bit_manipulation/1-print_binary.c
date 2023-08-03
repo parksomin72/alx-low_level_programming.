4517406ddd051397cd290d9c4e98700873610ed3
parksomin72 <fatimazahraealawi@gmail.com>
2023-08-03 03:37:16 -0700

    New commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,9 +1,16 @@
 #include "main.h"
 /**
+ * _putchar - Writes a character to the standard output (stdout).
+ * @c: The character to be written.
+ *
+ * Return: On success, the character written is returned.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+/**
  * print_binary - Prints the binary representation of a number.
  * @n: The number to be printed in binary representation.
  */
-int _putchar(char c);
 void print_binary(unsigned long int n)
 {
 if (n > 1)
