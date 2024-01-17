1444581259694de274c4447d83141bc7811b42c4
parksomin72 <fatimazahraealawi@gmail.com>
2024-01-16 17:44:38 -0800

    Jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -2,41 +2,49 @@
 #include <stdlib.h>
 #include <math.h>
 #include "search_algos.h"
-#define MIN(x, y) ((x) < (y) ? (x) : (y))
 
 /**
  * jump_search - Searches for a value in a sorted array using Jump search
  * @array: Pointer to the first element of the array to search in
- * @size: Number of elements in the array
+ * @size: Number of elements in array
  * @value: Value to search for
  *
- * Return:Index where the value is located,or-1 if not present or array is NULL
+ * Return: The first index where the value is located, or -1 if not found
  */
-
 int jump_search(int *array, size_t size, int value)
 {
-size_t step, prev, end, i;
+size_t step, prev, i;
 if (array == NULL)
 return (-1);
 step = sqrt(size);
 prev = 0;
-end = MIN(step, size);
 printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
-while (array[end - 1] < value)
+while (array[min(step, size) - 1] < value)
 {
 prev = step;
 step += sqrt(size);
 if (prev >= size)
 return (-1);
-end = MIN(step, size);
 printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
 }
-printf("Value found between indexes [%lu] and [%lu]\n", prev, end);
-for (i = prev; i < end; i++)
+printf("Value found between indexes [%lu] and [%lu]\n", prev, step);
+for (i = prev; i < min(step, size); i++)
 {
 printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 if (array[i] == value)
 return (i);
 }
 return (-1);
+}
+
+/**
+ * min - Returns the minimum of two values
+ * @a: First value
+ * @b: Second value
+ *
+ * Return: The minimum of a and b
+ */
+size_t min(size_t a, size_t b)
+{
+return ((a < b) ? a : b);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,4 +6,5 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
+size_t min(size_t a, size_t b);
 #endif /* SEARCH_ALGOS_H */
