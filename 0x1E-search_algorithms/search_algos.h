b8f836b2ea9f4c9ef4c06e757ade590b37d2971a
parksomin72 <fatimazahraealawi@gmail.com>
2024-01-17 15:09:35 -0800

    .

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -14,5 +14,6 @@ int exponential_search(int *array, size_t size, int value);
 int binary_search_helper(int *array, int value, size_t low, size_t high);
 int binary_search_recursion(int *array, int value, size_t low, size_t high);
 listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif /* SEARCH_ALGOS_H */
