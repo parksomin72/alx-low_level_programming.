05fed3d05a314f5e14cd55deb80610ada86b4ee4
parksomin72 <fatimazahraealawi@gmail.com>
2024-01-20 13:31:47 -0800

    Linear search in a skip list

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,48 +1,41 @@
 #include "search_algos.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * linear_skip - searches for a value conatined in a skip list; assumes a
- * list with sorted values and a single skip layer with nodes at every index
- * which is a multiple of the square root of the size of the list
+ * linear_skip - Searches for an algorithm in a sorted singly
+ *               linked list of integers using linear skip.
+ * @list: A pointer to the  head of the linked list to search.
+ * @value: The value to search for.
  *
- * @list: pointer to the head of the skip list to traverse
- * @value: value to search for
- * Return: pointer on the first node where value is located, or NULL if list
- * is NULL or value not found
+ * Return: If the value is not present or the head of the list is NULL, NULL.
+ *         Otherwise, a pointer to the first node where the value is located.
+ *
+ * Description: Prints a value every time it is compared in the list.
+ *              Uses the square root of the list size as the jump step.
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-skiplist_t *temp = NULL, *stop = NULL;
-if (!list)
+skiplist_t *node, *jump;
+if (list == NULL)
 return (NULL);
-temp = list;
-while (temp && temp->express && temp->express->n < value)
+for (node = jump = list; jump->next != NULL && jump->n < value;)
 {
-printf("Value checked at index [%lu] = [%i]\n",
-		temp->express->index, temp->express->n);
-temp = temp->express;
-}
-stop = temp;
-while (stop && stop->next != stop->express)
-stop = stop->next;
-if (temp->express)
+node = jump;
+if (jump->express != NULL)
 {
-printf("Value checked at index [%lu] = [%i]\n",
-		temp->express->index, temp->express->n);
-printf("Value found between indexes [%lu] and [%lu]\n",
-		temp->index, temp->express->index);
+jump = jump->express;
+printf("Value checked at index [%ld] = [%d]\n",
+jump->index, jump->n);
 }
 else
-printf("Value found between indexes [%lu] and [%lu]\n", temp->index, stop->index);
-while (temp != stop && temp->n < value)
 {
-printf("Value checked at index [%lu] = [%i]\n", temp->index, temp->n);
-temp = temp->next;
+while (jump->next != NULL)
+jump = jump->next;
 }
-printf("Value checked at index [%lu] = [%i]\n", temp->index, temp->n);
-if (temp == stop)
-return (NULL);
-return (temp);
+}
+printf("Value found between indexes [%ld] and [%ld]\n",
+node->index, jump->index);
+for (; node->index < jump->index && node->n < value; node = node->next)
+printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+return (node->n == value ? node : NULL);
 }
--- 0x1E-search_algorithms/skiplist/print_skiplist.c
+++ 0x1E-search_algorithms/skiplist/print_skiplist.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "search_algos.h"
+#include "../search_algos.h"
 
 /**
  * print_skiplist - dump the content of a skiplist_t
@@ -11,17 +11,16 @@
  */
 void print_skiplist(const skiplist_t *list)
 {
-	const skiplist_t *node;
-
-	printf("List :\n");
-	for (node = list; node; node = node->next)
-	{
-		printf("Index[%lu] = [%d]\n", node->index, node->n);
-	}
-	printf("\nExpress lane :\n");
-	for (node = list; node; node = node->express)
-	{
-		printf("Index[%lu] = [%d]\n", node->index, node->n);
-	}
-	printf("\n");
+const skiplist_t *node;
+printf("List :\n");
+for (node = list; node; node = node->next)
+{
+printf("Index[%lu] = [%d]\n", node->index, node->n);
+}
+printf("\nExpress lane :\n");
+for (node = list; node; node = node->express)
+{
+printf("Index[%lu] = [%d]\n", node->index, node->n);
+}
+printf("\n");
 }
